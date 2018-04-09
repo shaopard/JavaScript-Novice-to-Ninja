23b0f34d3a50dc25658869b1baa0015481440148
shaopard <adi_azrael@yahoo.com>
2018-04-09 09:56:17 +0300

    After chapter 5 - Objects

--- main.js
+++ main.js
@@ -1,34 +1,43 @@
+// Set the questions
 const quiz = [
-  ["What is Superman's real name?", "Clark Kent"],
-  ["What is Wonder Woman's real name?", "Diana Prince"],
-  ["What is Batman's real name?", "Bruce Wayne"]
+  ["What is Superman's real name?","Clark Kent"],
+  ["What is Wonderwoman's real name?","Dianna Prince"],
+  ["What is Batman's real name?","Bruce Wayne"]
 ];
 
-function start(quiz) {
-  let score = 0;
-  // main game loop
-  for (const [question, answer] of quiz) {
-    const response = ask(question);
-    check(response, answer);
-  }
-  // end of main game loop
-  gameOver();
-  // function declarations
-  function ask(question) {
-    return prompt(question);
-  }
+function start(quiz){
+// initialize score
+ let score = 0;
 
-  function check(response, answer) {
-    if (response === answer) {
-      alert('Correct!');
-      score++;
-    } else {
-      alert(`Wrong! The correct answer was ${answer}`);
-    }
-  }
+// main game loop
+for(const [question,answer] of quiz){
+  const response = ask(question);
+  check(response,answer);
+}
+// end of main game loop
 
-  function gameOver() {
-    alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+gameOver();
+
+// function declarations
+function ask(question){
+  return prompt(question);
+}
+
+function check(response,answer){
+  // check if answer is correct
+  if(response === answer){
+    alert('Correct!');
+    // increase score by 1
+    score++;
+  } else {
+    alert(`Wrong! The correct answer was ${answer}`);
   }
 }
+
+function gameOver(){
+  // At the end of the game, report the player's score
+  alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+}
+}
+
 start(quiz);
